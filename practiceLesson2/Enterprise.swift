6479741e83c94bc5e90bb23b8d82be00c97cdd69
Alexandr205 <sanchez.kalashnikov.2005@gmail.com>
2022-06-13 12:37:05 +0600

    n

---
+++ practiceLesson2/Enterprise.swift
@@ -0,0 +1,20 @@
+import Foundation
+
+class Enterprises{
+    var name: String = ""
+    var age: Int = 0
+    var gender: String = ""
+    var experience: String = ""
+    var position: String = ""
+    
+    init(name: String, age: Int, gender: String, experience: String, position: String){
+        self.name = name
+        self.age = age
+        self.gender = gender
+        self.experience = experience
+        self.position = position
+    }
+    
+    
+    
+}
