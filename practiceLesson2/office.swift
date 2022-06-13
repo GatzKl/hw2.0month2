176b050b788b8e2bb43e25a026e9ad0bb7b7bc97
Alexandr205 <sanchez.kalashnikov.2005@gmail.com>
2022-06-13 12:38:02 +0600

    b

---
+++ practiceLesson2/office.swift
@@ -0,0 +1,11 @@
+import Foundation
+
+class Office{
+    var title: String = ""
+    var year: Int = 0
+  
+    init(title: String, year: Int){
+        self.title = title
+        self.year = year
+    }
+}
